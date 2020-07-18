e82576de0a6a66fae6ef95b66f96defd5564a68a
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-18 17:03:08 +0300

    First commit

---
+++ ProPetsGatewayService/src/main/java/proPets/gateway/ProPetsGatewayServiceApplication.java
@@ -0,0 +1,18 @@
+package proPets.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+
+public class ProPetsGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ ProPetsGatewayService/src/test/java/proPets/gateway/ProPetsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package proPets.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
