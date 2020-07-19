3cf45f479cc08a7002c6779e43d04c06774d51a1
olgak_tdvqm06 <olgak_tdvqm06@10.100.102.9>
2020-07-19 21:28:31 +0300

    added route filter

--- ProPetsGatewayService/src/main/java/proPets/gateway/ProPetsGatewayServiceApplication.java
+++ ProPetsGatewayService/src/main/java/proPets/gateway/ProPetsGatewayServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import proPets.gateway.filters.pre.SimpleFilter;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -13,6 +16,11 @@ public class ProPetsGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProPetsGatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 
 }
